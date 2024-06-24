8ca90cef69a2fbfecf1da0bf3f6c8401134dbba8
Tehseen Ihsan <124780798+tehseenihsann@users.noreply.github.com>
2024-06-24 13:37:47 +0500

    gsap

--- script.js
+++ script.js
@@ -3,45 +3,81 @@
 var tl = gsap.timeline();
 
 tl.from(".section1", {
-  y: 0,
-  duration: 1,
-  opacity: 0,
-  ease: "power2.out",
-  stagger: 0.5,
-  delay: 0.5,
+	y: 0,
+	duration: 0.3,
+	opacity: 0,
+	ease: "power2.out",
+	stagger: 0.5,
+
 });
 
 tl.from("nav ul li", {
-  y: 0,
-  duration: 0.5,
-  opacity: 0,
-  ease: "power2.out",
-  stagger: 0.1,
+	y: 0,
+	duration: 0.5,
+	opacity: 0,
+	ease: "power2.out",
+	stagger: 0.1,
 });
 
 var navbar = document.querySelector(".section1");
 var links = document.querySelector("nav");
-var bars = document.querySelector(".menu-btn");
+var show_btn = document.querySelector(".menu-btn");
+var hide_btn = document.querySelector(".hide-menu");
 
-screen_change();
+$(document).ready(function () {
+	screen_change();
+})
 
 function screen_change() {
-  var navWidth = navbar.offsetWidth;
-  if (navWidth <= 768) {
-    links.style.visibility = "hidden";
-    links.style.height = "0px";
-    bars.style.visibility = "visible";
-    // console.log(navWidth);
-  }
-  if (navWidth > 768) {
-    links.style.visibility = "visible";
-    bars.style.visibility = "hidden";
-  }
+	var navWidth = navbar.offsetWidth;
+	if (navWidth <= 768) {
+		links.style.height = "0px";
+		links.style.visibility = "hidden";
+		hide_btn.style.visibility = "hidden";
+		show_btn.style.visibility = "visible";
+	}
+	if (navWidth > 768) {
+		links.style.visibility = "visible";
+		show_btn.style.visibility = "hidden";
+		hide_btn.style.visibility = "hidden";
+	}
 }
 
 function show_menu() {
-  console.log("Clicked");
-  links.style.visibility = "visible";
-  links.style.height = "max-content";
-  bars.style.visibility = "hidden";
+	links.style.visibility = "visible";
+	links.style.height = "max-content";
+	show_btn.style.visibility = "hidden";
+	hide_btn.style.visibility = "visible";
 }
+
+function hide_menu() {
+	links.style.visibility = "hidden";
+	links.style.height = "0px";
+	show_btn.style.visibility = "visible";
+	hide_btn.style.visibility = "hidden";
+}
+
+
+// section 2
+
+let headText = '';
+const mainHead = document.querySelector(".section2 .left h1");
+const mainText = mainHead.innerText;
+
+var splitText = mainText.split("");
+
+splitText.forEach(ch => {
+	headText += "<span>" + ch + "</span>";
+});
+
+document.querySelector(".section2 .left h1").innerHTML = headText;
+
+var tl2 = gsap.timeline();
+
+tl2.from(".section2 .left h1 span", {
+	opacity: 0,
+	duration: 1,
+	stagger: 0.1,
+})
+
+tl2
